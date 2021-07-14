0111c7ecc4970bdb112a427f613f2272318342b6
Coder-prathamesh <87426098+Coder-prathamesh@users.noreply.github.com>
2021-07-14 13:41:51 +0100

    Final

--- beetle.py
+++ beetle.py
@@ -29,3 +29,52 @@ def starting_head_or_legs():
             legs = 999 # Just a number   
     print("Got the Head")
     return legs
+
+def mouth_ant_eye(legs):
+    # To get mouth, antenna and eye.
+    acond,econd,mcond,lcond,total_done = False,False,False,False,False  #Initialize the conditions
+    ac,ec,mc,lc = 0,0,0,legs #Initialise the organ counts
+    p,q,r,s = False,False,False,False 
+    if (legs == 999):
+        lcond = True
+    while not total_done:
+        # Run this loop until we get all the parts
+        d  = generate_integer()
+        if(d==3 and not acond):
+            print('Antenna')
+            ac+=1
+        elif(d==4 and not econd):
+            print('Eyes')
+            ec+=1
+        elif(d==5 and not mcond):
+            print('Mouth')
+            mc+=1
+        elif(d==6 and not lcond):
+            print('Legs')
+            lc+=1
+        
+        if(ac == 2 and not p):
+            print("Got all the antennas")
+            acond = True
+            p = True
+        if(ec == 2 and not q):
+            print("Got all the eyes")
+            econd = True
+            q = True
+        if(mc == 1 and not r):
+            print("Got the mouth")
+            mcond = True
+            r = True
+        if(lc == 6 and not lcond and not s):
+            print("Got all the Legs")
+            lcond = True
+            s = True
+            
+        if(acond and econd and mcond and lcond):
+            print("###---Congratulation!!! You have got the WHOLE BODY---###")
+            total_done = True
+           
+starting_body()  
+legs = starting_head_or_legs()
+mouth_ant_eye(legs)
+
